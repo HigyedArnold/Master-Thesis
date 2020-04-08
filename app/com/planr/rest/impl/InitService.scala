343aa6124f0ee4272fb6606a1db687745f868011
Higyed Arnold <higyed_arnoldattila@yahoo.co.uk>
2020-04-08 14:15:17 +0300

    Load native library as eager singleton.

--- app/com/planr/rest/impl/InitService.scala
+++ app/com/planr/rest/impl/InitService.scala
@@ -1,8 +1,21 @@
 package com.planr.rest.impl
 
+import akka.actor.ActorSystem
 import com.google.inject.Inject
 import com.planr.api.sync.InitServiceT
+import com.planr.solver.util.NativeLibLoader
+import play.api.Configuration
 
-class InitService @Inject() () extends InitServiceT {
-  override def init(): Unit = ???
+class InitService @Inject() (config: Configuration, actorSystem: ActorSystem) extends InitServiceT {
+
+  init()
+
+  override def init(): Unit = {
+    // Load natives
+    if (NativeLibLoader.init(NativeLibLoader.DEVELOPMENT)) {
+      // Initialize actors
+
+    }
+    ()
+  }
 }
--- planr-solver/src/main/scala/com/planr/solver/util/NativeLibLoader.scala
+++ planr-solver/src/main/scala/com/planr/solver/util/NativeLibLoader.scala
@@ -1,13 +1,32 @@
 package com.planr.solver.util
 
+import play.api.Logger
+
+import scala.util.Try
+
 object NativeLibLoader {
 
+  private val logger    = Logger(this.getClass)
   private val separator = System.getProperty("file.separator")
 
   val DEVELOPMENT: String = separator + "target" + separator + "scala-2.13"
   val PRODUCTION:  String = separator + "app"
 
-  def init(path: String): Unit = System.load(System.getProperty("user.dir") + getLibrary(path))
+  def init(path: String): Boolean = {
+    val fullPath = System.getProperty("user.dir") + getLibrary(path)
+    Try {
+      System.load(fullPath)
+    }.fold(
+      _ => {
+        logger.error(s"Failed loading native library with path: $fullPath")
+        false
+      },
+      _ => {
+        logger.info(s"Success loading native library with path: $fullPath")
+        true
+      }
+    )
+  }
 
   private def getLibrary(path: String): String = {
     val os = System.getProperty("os.name").toLowerCase
