06f96a6623ec5e379656aa1685d21f8d80872db0
Higyed Arnold <higyed_arnoldattila@yahoo.co.uk>
2020-04-18 18:33:57 +0300

    .

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sbt build
 sbt docker
 
 # docker run com.planr/planr:latest
-#docker-compose up
-docker-compose up --scale planr=5
+# docker-compose up
+docker-compose -d up --scale planr=5
 
 # sbt -> project planr-gatling -> gatling:test
