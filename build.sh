69702823520439dfd3a2e4885c06f85bf327f81c
Higyed Arnold <higyed_arnoldattila@yahoo.co.uk>
2020-04-18 18:32:04 +0300

    Nginx

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ sbt build
 sbt docker
 
 # docker run com.planr/planr:latest
-docker-compose up
+# docker-compose up -d
+docker-compose up -d --scale planr=1
 
 # sbt -> project planr-gatling -> gatling:test
