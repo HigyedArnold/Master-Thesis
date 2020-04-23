2ccb5db2c8337198f05ce7ed0aed36d82a1e36ed
Higyed Arnold <higyed_arnoldattila@yahoo.co.uk>
2020-04-23 19:14:59 +0300

    .

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ sbt build
 sbt docker
 
 docker-compose up -d --scale planr=4
+# java -jar -Xmx512M -Xms512M -Xss1M planr-assembly-1.0.jar
 # docker-compose up -d --scale planr=1
+# java -jar -Xmx2048M -Xms2048M -Xss4M planr-assembly-4.0.jar
 
 # sbt -> project planr-gatling -> gatling:test
