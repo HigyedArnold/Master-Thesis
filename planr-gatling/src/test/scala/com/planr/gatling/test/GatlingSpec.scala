3f7b7c25f82250a0169651493d71f4fcb075007f
Higyed Arnold <higyed_arnoldattila@yahoo.co.uk>
2020-04-23 19:23:39 +0300

    Add warmup tests to gatling

--- planr-gatling/src/test/scala/com/planr/gatling/test/GatlingSpec.scala
+++ planr-gatling/src/test/scala/com/planr/gatling/test/GatlingSpec.scala
@@ -14,18 +14,39 @@ class GatlingSpec extends Simulation {
 
   // For options visit: https://gatling.io/docs/3.3/cheat-sheet/
   val scenarios = List(
+
 //    BaseScenarios.testScenario.inject(
 //      constantUsersPerSec(10).during(20 seconds)
 //    ),
+
 //    BaseScenarios.solveScenario.inject(
 //      constantUsersPerSec(100).during(50 seconds)
 //    ),
+
+    // WARMUP
+    // 1 Request / Second
+    BaseScenarios.solveScenario4.inject(
+      constantUsersPerSec(1).during(60 seconds)
+    ),
+
+    // 4 Requests / Second
+    BaseScenarios.solveScenario4_4.inject(
+      constantUsersPerSec(1).during(60 seconds)
+    )
+
+    // PERFORMANCE
+    // 80 Requests / Second
     BaseScenarios.solveScenario4.inject(
       constantUsersPerSec(20).during(20 seconds)
     ),
+
+    // 320 Requests / Second
     BaseScenarios.solveScenario4_4.inject(
       constantUsersPerSec(20).during(20 seconds)
     )
+
+    // ENDURANCE
+
   )
 
   setUp(scenarios)
